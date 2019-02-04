85330d4b6935453020d807a34a04fb9bdc1b636a
daratursina <32977759+daratursina@users.noreply.github.com>
2019-02-04 22:00:02 +0700

    Tugas1 PBKK G_Hello Word

---
+++ Tugas.js
@@ -0,0 +1,18 @@
+/*
+ * Main App file App.js
+ * @author Dara Tursina
+ */
+
+// Dependencies requirements, Express 4
+var express        = require('express');
+var app            = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(8080);
+console.log('Server started on port 8080');
+
+// First example router
+app.get('/', function(req, res) {
+    res.send("Hello world");
+});
